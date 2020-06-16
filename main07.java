ef843bb3fee63280bf672ab6a1b341ddbf3cf566
Lucas Monteiro Bastos <monteiro.bastos1994@gmail.com>
2020-06-16 00:23:10 -0300

    Valor a ser cobrado

---
+++ main07.java
@@ -0,0 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class main07 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int minutosConsumidos = sc.nextInt();
+		
+		double valorCobrado = 50.0;
+		
+		if(minutosConsumidos > 100) {
+			valorCobrado = valorCobrado + (minutosConsumidos - 100) * 2.0;
+		}
+		
+		System.out.printf("O valor a ser cobrado é: %.2f%n", valorCobrado);
+		
+		sc.close();
+	}
+
+}
